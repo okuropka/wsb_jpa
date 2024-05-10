450f9e532c6ad0ddfa2543e2019681943b515799
student-wroclaw <okuropka@gmail.com>
2024-05-10 14:24:46 +0200

    rozpoczęte 3 ostatnie testy

---
+++ wsb-jpa1/src/main/test/com/capgemini/wsb/persistance/dao/PatientDaoTest.java
@@ -0,0 +1,55 @@
+package com.capgemini.wsb.persistance.dao;
+
+import com.capgemini.wsb.persistence.dao.PatientDao;
+import com.capgemini.wsb.persistence.entity.PatientEntity;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PatientDaoTest
+{
+    @Autowired
+    private PatientDao patientDao;
+
+    @Transactional
+    @Test
+    public void testShouldFindPatientById() {
+        // given
+        // when
+        PatientEntity patientEntity = patientDao.findOne(1L);
+        // then
+        assertThat(patientEntity).isNotNull();
+        assertThat(patientEntity.getFirstName()).isEqualTo("imiePacjenta");
+    }
+    
+    @Transactional
+    @Test
+    public void testFindPatientsWithMoreVisitsThan() {
+        // given
+
+        // when
+        PatientEntity patientEntity = patientDao.findOne(1L);
+        // then
+
+    }
+
+    @Transactional
+    @Test
+    public void testFindFemalePatients() {
+        // given
+
+        // when
+        PatientEntity patientEntity = patientDao.findOne(1L);
+        // then
+
+    }
+
+
+}
